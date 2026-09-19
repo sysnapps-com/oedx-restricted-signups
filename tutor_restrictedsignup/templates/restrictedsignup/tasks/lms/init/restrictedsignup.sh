3e27eddb0d9e0a90fbaab962803044ca300231c7
Sysnapps <68092763+sysnapps@users.noreply.github.com>
2026-09-19 13:38:45 -0600

    Update restrictedsignup.sh
    updated sh file

--- tutor_restrictedsignup/templates/restrictedsignup/tasks/lms/init/restrictedsignup.sh
+++ tutor_restrictedsignup/templates/restrictedsignup/tasks/lms/init/restrictedsignup.sh
@@ -1,10 +1,9 @@
-{% if RESTRICTEDSIGNUP_FORCE_LEGACY_DASHBOARD %}
 echo "tutor-contrib-restrictedsignup: enabling instructor.legacy_instructor_dashboard waffle flag"
 ./manage.py lms shell -c "
 from waffle.models import Flag
 flag, _ = Flag.objects.get_or_create(name='instructor.legacy_instructor_dashboard')
 flag.everyone = True
 flag.save()
-print('instructor.legacy_instructor_dashboard set to: everyone=True')
+print('✓ Legacy dashboard enabled' if not created else '✓ Legacy dashboard flag created and enabled')
 "
-{% endif %}
+# instructor.legacy_instructor_dashboard set to: everyone=True
